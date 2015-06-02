0e20d73c7ce46a535c6faeecfd594fb7896f7d4f
Dan Lurie <danjlurie@gmail.com>
2015-06-02 14:56:19 -0700

    Removed manual correction for intensity normalization. This functionality is replicated by adding -orig_vol to 3dSkullStrip. Also added copious comments.

--- CPAC/anat_preproc/anat_preproc.py
+++ CPAC/anat_preproc/anat_preproc.py
@@ -1,6 +1,5 @@
 from nipype.interfaces.afni import preprocess
 import nipype.pipeline.engine as pe
-import nipype.interfaces.afni as afni
 import nipype.interfaces.utility as util
 
 
@@ -28,32 +27,28 @@ def create_anat_preproc(already_skullstripped=False):
     
     Workflow Outputs::
     
-        outputspec.refit : nifti file
-            Deobliqued anatomical data 
+        outputspec.deoblique : nifti file
+            Deobliqued anatomical image. 
         outputspec.reorient : nifti file
-            RPI oriented anatomical data 
+            RPI oriented anatomical image. 
         outputspec.skullstrip : nifti file
-            Skull Stripped RPI oriented mprage file with normalized intensities.
+            Skull-stripped anatomical image.
         outputspec.brain : nifti file
-            Skull Stripped RPI Brain Image with original intensity values and not normalized or scaled.
+            Skull-stripped anatomical image.
     
-    Order of commands:
+    Order of preprocessing steps and command-line equivalents:
 
-    - Deobliqing the scans.  For details see `3drefit <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3drefit.html>`_::
+    - Deoblique. For details see `3drefit <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3drefit.html>`_::
     
         3drefit -deoblique mprage.nii.gz
         
-    - Re-orienting the Image into Right-to-Left Posterior-to-Anterior Inferior-to-Superior  (RPI) orientation.  For details see `3dresample <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dresample.html>`_::
+    - Re-orient to RPI. For details see `3dresample <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dresample.html>`_::
     
         3dresample -orient RPI -prefix mprage_RPI.nii.gz -inset mprage.nii.gz 
     
-    - SkullStripping the image.  For details see `3dSkullStrip <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dSkullStrip.html>`_::
+    - Skull-strip. For details see `3dSkullStrip <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dSkullStrip.html>`_::
     
         3dSkullStrip -input mprage_RPI.nii.gz -o_ply mprage_RPI_3dT.nii.gz
-    
-    - The skull stripping step modifies the intensity values. To get back the original intensity values, we do an element wise product of RPI data with step function of skull Stripped data.  For details see `3dcalc <http://afni.nimh.nih.gov/pub/dist/doc/program_help/3dcalc.html>`_::
-    
-        3dcalc -a mprage_RPI.nii.gz -b mprage_RPI_3dT.nii.gz -expr 'a*step(b)' -prefix mprage_RPI_3dc.nii.gz
     
     High Level Workflow Graph:
     
@@ -75,55 +70,56 @@ def create_anat_preproc(already_skullstripped=False):
     >>> preproc.run() #doctest: +SKIP
             
     """
+
     preproc = pe.Workflow(name='anat_preproc')
-    inputNode = pe.Node(util.IdentityInterface(fields=['anat']),
-                        name='inputspec')
-    outputNode = pe.Node(util.IdentityInterface(fields=['refit',
-                                                    'reorient',
-                                                    'skullstrip',
-                                                    'brain']),
-                         name='outputspec')
-    anat_deoblique = pe.Node(interface=preprocess.Refit(),
-                         name='anat_deoblique')
+
+    """
+    Configure Workflow Nodes
+    """
+
+    # The input to this workflow is the anatomical image specified in the CPAC subject list.
+    inputNode = pe.Node(util.IdentityInterface(fields=['anat']), name='inputspec')
+    # This workflow outputs deobliqued, reoriented, and (if applicable) skull-stripped versions of the input image.
+    outputNode = pe.Node(util.IdentityInterface(fields=['deoblique', 'reorient', 'skullstrip', 'brain']), name='outputspec')
+
+    # Set up deoblique function.
+    anat_deoblique = pe.Node(interface=preprocess.Refit(), name='anat_deoblique')
     anat_deoblique.inputs.deoblique = True
-    anat_reorient = pe.Node(interface=preprocess.Resample(),
-                            name='anat_reorient')
+
+    # Set up reorient function.
+    anat_reorient = pe.Node(interface=preprocess.Resample(), name='anat_reorient')
     anat_reorient.inputs.orientation = 'RPI'
     anat_reorient.inputs.outputtype = 'NIFTI_GZ'
+
     if not already_skullstripped:
-        anat_skullstrip = pe.Node(interface=preprocess.SkullStrip(),
-                                  name='anat_skullstrip')
-        #anat_skullstrip.inputs.options = '-o_ply'
+        # Set up Skull Stripping
+        anat_skullstrip = pe.Node(interface=preprocess.SkullStrip(), name='anat_skullstrip')
+        # Keep intensity values the same in the output as in the input.
+        anat_skullstrip.inputs.options = '-orig_vol'
         anat_skullstrip.inputs.outputtype = 'NIFTI_GZ'
-    anat_skullstrip_orig_vol = pe.Node(interface=preprocess.Calc(),
-                        name='anat_skullstrip_orig_vol')
-    anat_skullstrip_orig_vol.inputs.expr = 'a*step(b)'
-    anat_skullstrip_orig_vol.inputs.outputtype = 'NIFTI_GZ'
-     
-    preproc.connect(inputNode, 'anat',
-                    anat_deoblique, 'in_file')
-    preproc.connect(anat_deoblique, 'out_file',
-                    anat_reorient, 'in_file')
+        
+    """
+    Connect Workflow Nodes
+    """
+
+    # Deoblique takes the 'raw' anatomical image as input.
+    preproc.connect(inputNode, 'anat', anat_deoblique, 'in_file')
+    # The deobliqued image is used as the input for Reorient.
+    preproc.connect(anat_deoblique, 'out_file', anat_reorient, 'in_file')
+    
     if not already_skullstripped:
-        preproc.connect(anat_reorient, 'out_file',
-                        anat_skullstrip, 'in_file')
-        preproc.connect(anat_skullstrip, 'out_file',
-                        anat_skullstrip_orig_vol, 'in_file_b')
+        # The reoriented image is used as the input for Skullstrip.
+        preproc.connect(anat_reorient, 'out_file', anat_skullstrip, 'in_file')
+    
+    # Output deobliqued and reoriented images.
+    preproc.connect(anat_deoblique, 'out_file', outputNode, 'deoblique')
+    preproc.connect(anat_reorient, 'out_file', outputNode, 'reorient')
+    
+    # Output skull-stripped images.
+    if not already_skullstripped:
+        preproc.connect(anat_skullstrip, 'out_file', outputNode, 'skullstrip')
+        preproc.connect(anat_skullstrip, 'out_file', outputNode, 'brain')
     else:
-        preproc.connect(anat_reorient, 'out_file',
-                        anat_skullstrip_orig_vol, 'in_file_b')
-    preproc.connect(anat_reorient, 'out_file',
-                    anat_skullstrip_orig_vol, 'in_file_a')
-   
-    preproc.connect(anat_deoblique, 'out_file',
-                    outputNode, 'refit')
-    preproc.connect(anat_reorient, 'out_file',
-                    outputNode, 'reorient')
-    if not already_skullstripped:
-        preproc.connect(anat_skullstrip, 'out_file',
-                        outputNode, 'skullstrip')
-    preproc.connect(anat_skullstrip_orig_vol, 'out_file',
-                        outputNode, 'brain')
+        preproc.connect(anat_reorient, 'out_file', outputNode, 'brain')
 
     return preproc
-
